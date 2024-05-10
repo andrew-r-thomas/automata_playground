41bc6e03a1435fd2195d75d9b96920e6c9369309
Andrew Thomas <andrew@andrewrthomas.com>
2024-05-10 15:25:48 -0700

    save me iced, please, i need you now more than ever

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use iced::Element;
+use iced::Sandbox;
+use iced::Settings;
+
+struct Playground {}
+
+impl Sandbox for Playground {
+    type Message = ();
+
+    fn new() -> Self {
+        Self {}
+    }
+
+    fn title(&self) -> String {
+        String::from("A cool application")
+    }
+
+    fn update(&mut self, _message: Self::Message) {
+        // This application has no interactions
+    }
+
+    fn view(&self) -> Element<Self::Message> {
+        "Hello, world!".into()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    Playground::run(Settings::default()).unwrap()
 }
