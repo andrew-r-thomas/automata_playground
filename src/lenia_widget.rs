c06a70006d08c1536d94ea258a81111adb8acf36
Andrew Thomas <andrew@andrewrthomas.com>
2024-05-18 14:11:00 -0700

    ok we have a brown triangle

--- src/lenia_widget.rs
+++ src/lenia_widget.rs
@@ -2,15 +2,21 @@ use iced::{
     mouse,
     widget::shader::{
         wgpu::{
-            self, include_wgsl,
+            self,
+            core::id::TextureViewId,
+            include_wgsl,
             util::{BufferInitDescriptor, DeviceExt},
-            BindGroupDescriptor, BindGroupEntry, BindGroupLayoutEntry, BindingResource,
-            BufferUsages, ComputePipeline, RenderPipeline, ShaderStages, Texture,
-            TextureDescriptor, TextureFormat, TextureUsages, TextureViewDescriptor,
+            BindGroup, BindGroupDescriptor, BindGroupEntry, BindGroupLayoutEntry, BindingResource,
+            BufferUsages, CommandEncoderDescriptor, ComputePassDescriptor, ComputePipeline,
+            RenderPipeline, ShaderStages, Texture, TextureDescriptor, TextureFormat, TextureUsages,
+            TextureView, TextureViewDescriptor,
         },
         Primitive, Program,
     },
 };
+
+const WORKGROUP_SIZE: u32 = 8;
+const GAME_SIZE: u32 = 512;
 
 #[derive(Debug)]
 pub struct LeniaPrimitive {}
@@ -22,15 +28,18 @@ impl Primitive for LeniaPrimitive {
         device: &iced::widget::shader::wgpu::Device,
         queue: &iced::widget::shader::wgpu::Queue,
         _bounds: iced::Rectangle,
-        target_size: iced::Size<u32>,
+        _target_size: iced::Size<u32>,
         _scale_factor: f32,
         storage: &mut iced::widget::shader::Storage,
     ) {
         if !storage.has::<LeniaPipeline>() {
             storage.store(LeniaPipeline::new(device, queue, format));
         }
-        todo!()
-        // put the pipepline in here for setting up the random board etc
+
+        let lenia = storage.get_mut::<LeniaPipeline>().unwrap();
+        let mut encoder = device.create_command_encoder(&CommandEncoderDescriptor::default());
+
+        lenia.setup_board(&mut encoder);
     }
 
     fn render(
@@ -41,10 +50,8 @@ impl Primitive for LeniaPrimitive {
         _viewport: iced::Rectangle<u32>,
         encoder: &mut iced::widget::shader::wgpu::CommandEncoder,
     ) {
-        let lenia_compute = storage.get::<LeniaPipeline>().unwrap();
-        lenia_compute.calc_step(encoder);
-
-        todo!()
+        let pipe = storage.get::<LeniaPipeline>().unwrap();
+        pipe.render(encoder, target);
     }
 }
 
@@ -65,194 +72,231 @@ impl<Message> Program<Message> for LeniaProgram {
 }
 
 struct LeniaPipeline {
-    init_pipeline: ComputePipeline,
-    update_pipeline: ComputePipeline,
+    // init_pipeline: ComputePipeline,
+    // update_pipeline: ComputePipeline,
     render_pipeline: RenderPipeline,
-    game_buff_a: Texture,
-    game_buff_b: Texture,
+    // game_buff_a: Texture,
+    // game_buff_b: Texture,
+    // bind_group: BindGroup,
 }
 
 impl LeniaPipeline {
     pub fn new(device: &wgpu::Device, queue: &wgpu::Queue, format: wgpu::TextureFormat) -> Self {
         let shader = device.create_shader_module(include_wgsl!("shaders/lenia.wgsl"));
-        let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-            label: None,
-            entries: &[
-                BindGroupLayoutEntry {
-                    binding: 0,
-                    visibility: ShaderStages::COMPUTE | ShaderStages::VERTEX_FRAGMENT,
-                    ty: wgpu::BindingType::StorageTexture {
-                        access: wgpu::StorageTextureAccess::ReadWrite,
-                        format: TextureFormat::R32Float,
-                        view_dimension: wgpu::TextureViewDimension::D2,
-                    },
-                    count: None,
-                },
-                BindGroupLayoutEntry {
-                    binding: 1,
-                    visibility: ShaderStages::COMPUTE | ShaderStages::VERTEX_FRAGMENT,
-                    ty: wgpu::BindingType::StorageTexture {
-                        access: wgpu::StorageTextureAccess::ReadWrite,
-                        format: TextureFormat::R32Float,
-                        view_dimension: wgpu::TextureViewDimension::D2,
-                    },
-                    count: None,
-                },
-                BindGroupLayoutEntry {
-                    binding: 2,
-                    visibility: ShaderStages::COMPUTE | ShaderStages::VERTEX_FRAGMENT,
-                    ty: wgpu::BindingType::Buffer {
-                        ty: wgpu::BufferBindingType::Uniform,
-                        has_dynamic_offset: false,
-                        min_binding_size: None,
-                    },
-                    count: None,
-                },
-            ],
-        });
+        // let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+        //     label: None,
+        //     entries: &[
+        //         BindGroupLayoutEntry {
+        //             binding: 0,
+        //             visibility: ShaderStages::COMPUTE | ShaderStages::VERTEX_FRAGMENT,
+        //             ty: wgpu::BindingType::StorageTexture {
+        //                 access: wgpu::StorageTextureAccess::WriteOnly,
+        //                 format: TextureFormat::R32Float,
+        //                 view_dimension: wgpu::TextureViewDimension::D2,
+        //             },
+        //             count: None,
+        //         },
+        //         BindGroupLayoutEntry {
+        //             binding: 1,
+        //             visibility: ShaderStages::COMPUTE | ShaderStages::VERTEX_FRAGMENT,
+        //             ty: wgpu::BindingType::StorageTexture {
+        //                 access: wgpu::StorageTextureAccess::WriteOnly,
+        //                 format: TextureFormat::R32Float,
+        //                 view_dimension: wgpu::TextureViewDimension::D2,
+        //             },
+        //             count: None,
+        //         },
+        //         BindGroupLayoutEntry {
+        //             binding: 2,
+        //             visibility: ShaderStages::COMPUTE | ShaderStages::VERTEX_FRAGMENT,
+        //             ty: wgpu::BindingType::Buffer {
+        //                 ty: wgpu::BufferBindingType::Uniform,
+        //                 has_dynamic_offset: false,
+        //                 min_binding_size: None,
+        //             },
+        //             count: None,
+        //         },
+        //     ],
+        // });
 
-        let game_buff_a = device.create_texture(&TextureDescriptor {
-            label: Some("game buff a"),
-            size: wgpu::Extent3d {
-                width: 512,
-                height: 512,
-                depth_or_array_layers: 0,
-            },
-            mip_level_count: 0,
-            sample_count: 1,
-            dimension: wgpu::TextureDimension::D2,
-            format: TextureFormat::R32Float,
-            usage: TextureUsages::TEXTURE_BINDING | TextureUsages::COPY_DST,
-            view_formats: &[],
-        });
-        let game_buff_b = device.create_texture(&TextureDescriptor {
-            label: Some("game buff b"),
-            size: wgpu::Extent3d {
-                width: 512,
-                height: 512,
-                depth_or_array_layers: 0,
-            },
-            mip_level_count: 0,
-            sample_count: 1,
-            dimension: wgpu::TextureDimension::D2,
-            format: TextureFormat::R32Float,
-            usage: TextureUsages::TEXTURE_BINDING | TextureUsages::COPY_DST,
-            view_formats: &[],
-        });
+        // let game_buff_a = device.create_texture(&TextureDescriptor {
+        //     label: Some("game buff a"),
+        //     size: wgpu::Extent3d {
+        //         width: 512,
+        //         height: 512,
+        //         depth_or_array_layers: 0,
+        //     },
+        //     mip_level_count: 0,
+        //     sample_count: 1,
+        //     dimension: wgpu::TextureDimension::D2,
+        //     format: TextureFormat::R32Float,
+        //     usage: TextureUsages::TEXTURE_BINDING | TextureUsages::COPY_DST,
+        //     view_formats: &[],
+        // });
+        // let game_buff_b = device.create_texture(&TextureDescriptor {
+        //     label: Some("game buff b"),
+        //     size: wgpu::Extent3d {
+        //         width: 512,
+        //         height: 512,
+        //         depth_or_array_layers: 0,
+        //     },
+        //     mip_level_count: 0,
+        //     sample_count: 1,
+        //     dimension: wgpu::TextureDimension::D2,
+        //     format: TextureFormat::R32Float,
+        //     usage: TextureUsages::TEXTURE_BINDING | TextureUsages::COPY_DST,
+        //     view_formats: &[],
+        // });
 
-        let game_view_a = game_buff_a.create_view(&TextureViewDescriptor {
-            label: Some("game view a"),
-            format: Some(TextureFormat::R32Float),
-            base_mip_level: 0,
-            mip_level_count: Some(1),
-            ..Default::default()
-        });
-        let game_view_b = game_buff_b.create_view(&TextureViewDescriptor {
-            label: Some("game view a"),
-            format: Some(TextureFormat::R32Float),
-            base_mip_level: 0,
-            mip_level_count: Some(1),
-            ..Default::default()
-        });
+        // let game_view_a = game_buff_a.create_view(&TextureViewDescriptor {
+        //     label: Some("game view a"),
+        //     format: Some(TextureFormat::R32Float),
+        //     base_mip_level: 0,
+        //     mip_level_count: Some(1),
+        //     ..Default::default()
+        // });
+        // let game_view_b = game_buff_b.create_view(&TextureViewDescriptor {
+        //     label: Some("game view a"),
+        //     format: Some(TextureFormat::R32Float),
+        //     base_mip_level: 0,
+        //     mip_level_count: Some(1),
+        //     ..Default::default()
+        // });
 
-        let input_view_buff = device.create_buffer_init(&BufferInitDescriptor {
-            label: Some("which is input"),
-            contents: bytemuck::cast_slice(&[0]),
-            usage: BufferUsages::UNIFORM | BufferUsages::COPY_DST,
-        });
+        // let input_view_buff = device.create_buffer_init(&BufferInitDescriptor {
+        //     label: Some("which is input"),
+        //     contents: bytemuck::cast_slice(&[0]),
+        //     usage: BufferUsages::UNIFORM | BufferUsages::COPY_DST,
+        // });
 
-        let bind_group = device.create_bind_group(&BindGroupDescriptor {
-            label: Some("storage bind group"),
-            layout: &bind_group_layout,
-            entries: &[
-                BindGroupEntry {
-                    binding: 0,
-                    resource: BindingResource::TextureView(&game_view_a),
-                },
-                BindGroupEntry {
-                    binding: 1,
-                    resource: BindingResource::TextureView(&game_view_b),
-                },
-                BindGroupEntry {
-                    binding: 2,
-                    resource: input_view_buff.as_entire_binding(),
-                },
-            ],
-        });
+        // let bind_group = device.create_bind_group(&BindGroupDescriptor {
+        //     label: Some("storage bind group"),
+        //     layout: &bind_group_layout,
+        //     entries: &[
+        //         BindGroupEntry {
+        //             binding: 0,
+        //             resource: BindingResource::TextureView(&game_view_a),
+        //         },
+        //         BindGroupEntry {
+        //             binding: 1,
+        //             resource: BindingResource::TextureView(&game_view_b),
+        //         },
+        //         BindGroupEntry {
+        //             binding: 2,
+        //             resource: input_view_buff.as_entire_binding(),
+        //         },
+        //     ],
+        // });
 
         let layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
             label: Some("pipeline layout"),
-            bind_group_layouts: &[&bind_group_layout],
+            bind_group_layouts: &[
+                // &bind_group_layout
+            ],
             push_constant_ranges: &[],
         });
 
-        let init_pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
-            label: Some("lenia init"),
-            layout: Some(&layout),
-            module: &shader,
-            entry_point: "init",
-        });
+        // let init_pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+        //     label: Some("lenia init"),
+        //     layout: Some(&layout),
+        //     module: &shader,
+        //     entry_point: "init",
+        // });
 
-        let update_pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
-            label: Some("lenia compute"),
-            layout: Some(&layout),
-            module: &shader,
-            entry_point: "update",
-        });
+        // let update_pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+        //     label: Some("lenia compute"),
+        //     layout: Some(&layout),
+        //     module: &shader,
+        //     entry_point: "update",
+        // });
 
         let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-            label: Some("lenia render"),
+            label: Some("Render Pipeline"),
             layout: Some(&layout),
             vertex: wgpu::VertexState {
                 module: &shader,
-                entry_point: "render_vs",
+                entry_point: "vs_main",
                 buffers: &[],
             },
-            primitive: wgpu::PrimitiveState::default(),
-            depth_stencil: None,
-            multisample: wgpu::MultisampleState::default(),
             fragment: Some(wgpu::FragmentState {
                 module: &shader,
-                entry_point: "render_fs",
-                targets: &[],
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
             }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: Some(wgpu::Face::Back),
+                polygon_mode: wgpu::PolygonMode::Fill,
+                unclipped_depth: false,
+                conservative: false,
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
             multiview: None,
         });
 
         Self {
-            update_pipeline,
-            init_pipeline,
+            // update_pipeline,
+            // init_pipeline,
             render_pipeline,
-            game_buff_a,
-            game_buff_b,
+            // game_buff_a,
+            // game_buff_b,
+            // bind_group,
         }
     }
 
-    pub fn setup_board() {
-        todo!()
+    pub fn setup_board(&self, encoder: &mut wgpu::CommandEncoder) {
+        {
+            // let mut pass = encoder.begin_compute_pass(&ComputePassDescriptor::default());
+            // pass.set_bind_group(0, &self.bind_group, &[]);
+            // pass.set_pipeline(&self.init_pipeline);
+            // pass.dispatch_workgroups(GAME_SIZE / WORKGROUP_SIZE, GAME_SIZE / WORKGROUP_SIZE, 1);
+        }
     }
 
     pub fn calc_step(&self, encoder: &mut wgpu::CommandEncoder) {
-        let mut pass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
-            label: None,
-            timestamp_writes: None,
-        });
-        pass.set_pipeline(&self.update_pipeline);
+        // let mut pass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
+        //     label: None,
+        //     timestamp_writes: None,
+        // });
+        // pass.set_pipeline(&self.update_pipeline);
         todo!()
     }
 
-    pub fn render(&self, encoder: &mut wgpu::CommandEncoder) {
+    pub fn render(&self, encoder: &mut wgpu::CommandEncoder, view: &TextureView) {
         {
-            let mut pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor::default());
+            let mut pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                occlusion_query_set: None,
+                timestamp_writes: None,
+            });
+            // pass.set_bind_group(0, &self.bind_group, &[]);
             pass.set_pipeline(&self.render_pipeline);
-            let view = self
-                .game_buff_a
-                .create_view(&TextureViewDescriptor::default());
+            pass.draw(0..3, 0..1);
         }
-        todo!()
     }
 }
-
-struct LeniaRender {}
-
-impl LeniaRender {}
--- src/shaders/lenia.wgsl
+++ src/shaders/lenia.wgsl
@@ -1,19 +1,27 @@
-@vertex
-fn render_vs(@builtin(vertex_index) in_vertex_index: u32) -> @builtin(position) {
-    
+struct VertexOutput {
+    @builtin(position) clip_position: vec4<f32>,
+    @location(0) vert_pos: vec3<f32>,
 }
 
-// The shader reads the previous frame's state from the `input` texture, and writes the new state of
-// each pixel to the `output` texture. The textures are flipped each step to progress the
-// simulation.
-// Two textures are needed for the game of life as each pixel of step N depends on the state of its
-// neighbors at step N-1.
+@vertex
+fn vs_main(
+    @builtin(vertex_index) in_vertex_index: u32,
+) -> VertexOutput {
+    var out: VertexOutput;
+    let x = f32(1 - i32(in_vertex_index)) * 0.5;
+    let y = f32(i32(in_vertex_index & 1u) * 2 - 1) * 0.5;
+    out.clip_position = vec4<f32>(x, y, 0.0, 1.0);
+    out.vert_pos = out.clip_position.xyz;
+    return out;
+}
 
-@group(0) @binding(0) var input: texture_storage_2d<r32float, read>;
+@fragment
+fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
+    return vec4<f32>(0.3, 0.2, 0.1, 1.0);
+}
+
+@group(0) @binding(0) var input: texture_storage_2d<r32float, write>;
 @group(0) @binding(1) var output: texture_storage_2d<r32float, write>;
-
-// @group(0) @binding(2) var lir: storage_buffer<r32float>;
-// @group(0) @binding(3) var rir: storage_buffer<r32float>;
 
 fn hash(value: u32) -> u32 {
     var state = value;
@@ -57,31 +65,31 @@ fn init(@builtin(global_invocation_id) invocation_id: vec3<u32>) {
 
 }
 
-@compute @workgroup_size(8, 8, 1)
-fn update(@builtin(global_invocation_id) invocation_id: vec3<u32>) {
-    let location = vec2<i32>(i32(invocation_id.x), i32(invocation_id.y));
+// @compute @workgroup_size(8, 8, 1)
+// fn update(@builtin(global_invocation_id) invocation_id: vec3<u32>) {
+//     let location = vec2<i32>(i32(invocation_id.x), i32(invocation_id.y));
 
-    var sum: f32 = 0.0;
-    var total: f32 = 0.0;
-    for (var x: i32 = -i32(R); x<=i32(R); x++) {
-        for (var y: i32 = -i32(R); y<=i32(R); y++)
-        {
-            let r: f32 = sqrt(f32(x*x + y*y)) / R;
-            let txy: vec2<i32> = (location + vec2<i32>(x, y));
-            let val: f32 = textureLoad(input, txy).r;
-            let weight: f32 = bell(r, rho, omega);
-            sum += val * weight;
-            total += weight;
-        }
-    }
+//     var sum: f32 = 0.0;
+//     var total: f32 = 0.0;
+//     for (var x: i32 = -i32(R); x<=i32(R); x++) {
+//         for (var y: i32 = -i32(R); y<=i32(R); y++)
+//         {
+//             let r: f32 = sqrt(f32(x*x + y*y)) / R;
+//             let txy: vec2<i32> = (location + vec2<i32>(x, y));
+//             let val: f32 = textureLoad(input, txy).r;
+//             let weight: f32 = bell(r, rho, omega);
+//             sum += val * weight;
+//             total += weight;
+//         }
+//     }
 
-    let avg: f32 = sum / total;
-    let g: f32 = bell(avg, mu, sigma) * 2.0 - 1.0;
-    let val: f32 = textureLoad(input, location).r;
-    let c: f32 = clamp(val + dt * g, 0.0, 1.0);
+//     let avg: f32 = sum / total;
+//     let g: f32 = bell(avg, mu, sigma) * 2.0 - 1.0;
+//     let val: f32 = textureLoad(input, location).r;
+//     let c: f32 = clamp(val + dt * g, 0.0, 1.0);
 
-    let color = vec4<f32>(c, c, c, 1.0);
+//     let color = vec4<f32>(c, c, c, 1.0);
 
-    textureStore(output, location, color);
-}
+//     textureStore(output, location, color);
+// }
 
